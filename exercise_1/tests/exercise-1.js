352b7d7421c0e6380e1c8f793c4d040dfb05e3f1
Adrian <total_play2016@hotmail.com>
2025-01-25 13:49:29 -0600

    WIP

--- exercise_1/tests/exercise-1.js
+++ exercise_1/tests/exercise-1.js
@@ -3,8 +3,8 @@
  * No se permite usar objetos Date
  * Fórmula: años * 365 (ignoramos años bisiestos)
  *
- * @param {number} años - La edad en años
- * @returns {number} La edad en días
+ * @param {} años - La edad en años
+ * @returns {} La edad en días
  *
  * @example
  * // Para 1 año, se espera 365
@@ -18,8 +18,12 @@
  * // Para 10 años, se espera 3650
  * edadEnDias(10) // 3650
  */
-const edadEnDias = (años) => {
-  // TODO: Implementar la función
+function edadEnDias(años){
+return años * 365;
 }
 
-export default edadEnDias
+resultado = edadEnDias(1)
+console.log(resultado)    
+
+
+
